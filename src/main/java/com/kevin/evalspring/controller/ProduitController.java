608ee4c2d2106af10aa377d0411dd1c77b9013e3
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-19 11:49:56 +0100

    fix : partie n°5 > CategorieController ProduitController pointaient vers la même route

--- src/main/java/com/kevin/evalspring/controller/ProduitController.java
+++ src/main/java/com/kevin/evalspring/controller/ProduitController.java
@@ -7,9 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
-@RestController("/api")
+@RestController
 public class ProduitController {
     @Autowired
     private ProduitService produitService;
